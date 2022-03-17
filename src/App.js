e5f9bce380435b83bd3658e0801f6e330d8496ca
kaizen neri <46342330+kaizenneri01@users.noreply.github.com>
2022-03-18 00:09:23 +0800

    updating css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import Postcards from './components/Postcards';
-import './App.css';
-import SignupModal from './components/SignupModal';
-import { database } from './Firebase';
-import UploadPhotos from './components/UploadPhotos';
+import React, { useEffect, useState } from "react";
+import Postcards from "./components/Postcards";
+import "./App.css";
+import SignupModal from "./components/SignupModal";
+import { database } from "./Firebase";
+import UploadPhotos from "./components/UploadPhotos";
 
 function App() {
   const [postData, setPostData] = useState([]);
   const headerImage =
-    'https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png';
-  const [user, setUser] = useState('');
+    "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png";
+  const [user, setUser] = useState("");
 
-  const [pass, setPass] = useState('');
+  const [pass, setPass] = useState("");
 
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
-    database.collection('postData').onSnapshot((snapshot) => {
+    database.collection("postData").onSnapshot((snapshot) => {
       setPostData(snapshot.docs.map((doc) => doc.data()));
     });
   }, []);
@@ -35,9 +35,15 @@ function App() {
           email={email}
         />
       </div>
-      {postData.map((x) => (
-        <Postcards username={x.username} image={x.image} comment={x.comment} />
-      ))}
+      <div className="app__container">
+        {postData.map((x) => (
+          <Postcards
+            username={x.username}
+            image={x.image}
+            comment={x.comment}
+          />
+        ))}
+      </div>
     </div>
   );
 }
